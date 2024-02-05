5be500493db75adec915752d8ac68327664f9ff9
kauderk <estudio1kau@gmail.com>
2024-02-05 11:12:39 -0500

    explain unknown behavior

--- README.md
+++ README.md
@@ -1,51 +1,17 @@
-# [Update]
+to reproduce `npm run build`
 
-For learning how to create Chrome extension by Elm, I forked this repo and updated it to Elm 0.19.
+```js
+const webpack = require('webpack')
+const config = require('./background/webpack.config.js')
+webpack(config, () => {
+  // ...
+})
 
-# elm-chrome-extension
+// vs
 
-This experimental project demonstrates a Chrome extension composed of
-three Elm applications that communicate through Chrome's
-message passing.
-
-![screenshot](https://i.imgur.com/Y8Z0Wwn.png)
-
-The three apps:
-
-1. The persistent background app that runs globally for the duration
-   of the browser session.
-   - It holds the state and the other apps subscribe to state updates.
-2. The content-script app runs on each website tab.
-3. The popup app that runs each time you click the extension button.
-
-The content-script and popup apps use one-way messaging to trigger
-actions in the background app.
-
-Every time the background app model updates, it broadcasts to all
-listeners which replace their model with the new version.
-
-## The Demo
-
-This demo syncs up a simple model that tracks global mouse clicks:
-
-``` elm
-type alias Model =
-    { clicks : Int }
+const webpack = require('webpack')
+const config = require('./background/webpack.config.js')
+webpack(config)
 ```
 
-A click in any tab and a click in the popup app will update the
-model for all tabs/popup apps.
-
-## Development
-
-Git clone and then `npm install`.
-
-Build the extension into the `dist/` folder:
-
-    npm run build
-
-Build to `dist/` and rebuild on file changes:
-
-    npm run watch
-
-Once built, load the `dist/` folder as an extension in Chrome's UI.
+when I place a callback, which I need to do work, it opens the webpack output in a new text editor window, how to avoid that
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,56 +1,13 @@
-// @ts-check
-const gulp = require('gulp')
-const loadPlugins = require('gulp-load-plugins')
 const webpack = require('webpack')
-const del = require('del')
-
-/**
- * @type {{rename: import('gulp-rename'); util: import('gulp-util'); }}
- */
-const plugins = loadPlugins()
-
-const webpackConfig = {
-  background: require('./background/webpack.config.js'),
-  popup: require('./popup/webpack.config.js'),
-  content: require('./content/webpack.config.js'),
-}
-
-const scriptsTasks = Object.keys(webpackConfig)
-for (const appName of scriptsTasks) {
-  gulp.task(appName, (cb) => {
-    webpack(webpackConfig[appName], (err, stats) => {
-      // if (err) {
-      //   throw new plugins.util.PluginError('webpack', err)
-      // }
-      // if (!stats) {
-      //   throw new Error('[webpack]' + ' No stats')
-      // }
-      // plugins.util.log('[webpack]', stats.toString())
-      cb()
-    })
-  })
-}
-gulp.task('popup_html', () => {
-  return gulp
-    .src('popup/src/index.html')
-    .pipe(plugins.rename('popup.html'))
-    .pipe(gulp.dest('./dist'))
-})
-gulp.task('copy_manifest', () => {
-  return gulp.src('manifest.json').pipe(gulp.dest('./dist'))
+const config = require('./background/webpack.config.js')
+webpack(config, () => {
+  // ...
 })
 
-function build() {
-  return gulp.parallel('popup_html', 'copy_manifest', ...scriptsTasks)
-}
-function watch() {
-  gulp.watch(['popup/**/*', 'content/**/*', 'background/**/*'], build())
-}
-function clean() {
-  return del(['dist'])
-}
+// vs
 
-exports.build = build()
-exports.watch = watch
-exports.clean = clean
-exports.default = build()
+// const webpack = require('webpack')
+// const config = require('./background/webpack.config.js')
+// webpack(config)
+
+// when I place a callback, which I need to do work, it opens the webpack output in a new text editor window, how to avoid that
